a1fb452a83218a48bdcfda3013b04d12cf60ca88
sgc109 <sgc109109@gmail.com>
2018-01-28 04:08:18 +0900

    added FoodReview Model added FoodReview Activity added FoodReview insert code to Firebase Realtime DB

--- app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lifekau.android.lifekau;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = FoodReviewActivity.newIntent(this, FoodReviewActivity.RESTAURENT_TYPE_STUDENT);
+        startActivity(intent);
     }
 }
