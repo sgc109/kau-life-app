1ae1840cd07bbe942fa18c2157943124a5cdc710
sgc109 <sgc109109@gmail.com>
2018-01-31 16:06:36 +0900

    학식 리뷰 리스트 UI좀 손보고, 툴바 생성 및 네비게이션버튼 활성화

--- app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
@@ -3,13 +3,28 @@ package com.lifekau.android.lifekau;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = FoodReviewListActivity.newIntent(this, FoodReviewListActivity.RESTAURENT_TYPE_STUDENT);
-        startActivity(intent);
+        mButton = (Button)findViewById(R.id.main_activity_button);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_activity_button:
+                Intent intent = FoodReviewListActivity.newIntent(this, FoodReviewListActivity.RESTAURENT_TYPE_STUDENT);
+                startActivity(intent);
+            default:
+                return;
+        }
     }
 }
