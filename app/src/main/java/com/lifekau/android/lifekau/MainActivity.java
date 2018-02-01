ec6b7575cc9b4f32558eae44e06b83a6cfe0cb60
taemin <cprayer@naver.com>
2018-02-01 15:07:12 +0900

    LMS 로그인 처리(그 외 나머지 기능 작업중)

--- app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lifekau.android.lifekau;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LMSInfomationActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
