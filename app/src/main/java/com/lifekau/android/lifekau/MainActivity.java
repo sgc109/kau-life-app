eadeff4af1a0357c26e13c5735374a7d1970f320
sgc109 <sgc109109@gmail.com>
2018-02-01 21:05:10 +0900

    마스터브랜치랑 학식리뷰, 강의리뷰 리베이스하고 접합부좀 만짐

--- app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/MainActivity.java
@@ -7,22 +7,30 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button mButton;
+    private Button mButton1;
+    private Button mButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mButton = (Button)findViewById(R.id.main_activity_button);
-        mButton.setOnClickListener(this);
+        mButton1 = (Button) findViewById(R.id.main_activity_button1);
+        mButton1.setOnClickListener(this);
+        mButton2 = (Button) findViewById(R.id.main_activity_button2);
+        mButton2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.main_activity_button:
-                Intent intent = FoodReviewListActivity.newIntent(this, FoodReviewListActivity.RESTAURENT_TYPE_STUDENT);
-                startActivity(intent);
+            case R.id.main_activity_button1:
+                Intent intent1 = FoodReviewListActivity.newIntent(this, FoodReviewListActivity.RESTAURENT_TYPE_STUDENT);
+                startActivity(intent1);
+                break;
+            case R.id.main_activity_button2:
+                Intent intent2 = LectureReviewSearchActivity.newIntent(this);
+                startActivity(intent2);
+                break;
             default:
                 return;
         }
