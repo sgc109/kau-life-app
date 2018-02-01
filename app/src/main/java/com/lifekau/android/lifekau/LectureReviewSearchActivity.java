a4118119e7416519b29f88cad9de219ba171910c
sgc109 <sgc109109@gmail.com>
2018-02-01 15:27:38 +0900

    강의 평가 기능에 필요한 모델, 액티비티 생성

---
+++ app/src/main/java/com/lifekau/android/lifekau/LectureReviewSearchActivity.java
@@ -0,0 +1,13 @@
+package com.lifekau.android.lifekau;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LectureReviewSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lecture_review_search);
+    }
+}
