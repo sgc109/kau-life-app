928b8c47fb6b2881e8a4c4c2b40cc6af90e1002f
sgc109 <sgc109109@gmail.com>
2018-02-01 17:45:03 +0900

    자동 완성 검색바 만드는 도중 커밋

--- app/src/main/java/com/lifekau/android/lifekau/LectureReviewSearchActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/LectureReviewSearchActivity.java
@@ -2,12 +2,24 @@ package com.lifekau.android.lifekau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class LectureReviewSearchActivity extends AppCompatActivity {
+
+    AutoCompleteTextView mAutoCompleteSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture_review_search);
+
+        String[] strs = new String[40];
+        for(int i = 0; i < 20; i++) strs[i] = "a a";
+        for(int i = 0; i < 20; i++) strs[i + 20] = "aa";
+        int layoutItemId = android.R.layout.simple_dropdown_item_1line;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layoutItemId, strs);
+
+
     }
 }
