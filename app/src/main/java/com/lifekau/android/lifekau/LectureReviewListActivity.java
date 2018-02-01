f09aa1886929f06188ba4e80db749f78617b978c
sgc109 <sgc109109@gmail.com>
2018-02-01 20:37:15 +0900

    강의리뷰 검색바 자동완성 완료(파베만 연동하면될듯)

--- app/src/main/java/com/lifekau/android/lifekau/LectureReviewListActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/LectureReviewListActivity.java
@@ -1,13 +1,27 @@
 package com.lifekau.android.lifekau;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LectureReviewListActivity extends AppCompatActivity {
+    private static final String EXTRA_LECTURE_NAME = "extra_lecture_name";
+    private String mLectureName;
 
+    public static Intent newIntent(Context context, String lectureName){
+        Intent intent = new Intent(context, LectureReviewListActivity.class);
+        intent.putExtra(EXTRA_LECTURE_NAME, lectureName);
+        return intent;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture_review_list);
+
+        Intent intent = getIntent();
+        mLectureName = intent.getStringExtra(EXTRA_LECTURE_NAME);
+        Toast.makeText(this, mLectureName, Toast.LENGTH_LONG).show();
     }
 }
