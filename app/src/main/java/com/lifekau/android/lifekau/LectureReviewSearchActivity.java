20c8217edfef2866cd04c220985fd73b0ffd76f8
sgc109 <sgc109109@gmail.com>
2018-02-01 20:37:15 +0900

    강의리뷰 검색바 자동완성 완료(파베만 연동하면될듯)

--- app/src/main/java/com/lifekau/android/lifekau/LectureReviewSearchActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/LectureReviewSearchActivity.java
@@ -4,31 +4,56 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class LectureReviewSearchActivity extends AppCompatActivity {
+public class LectureReviewSearchActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private AutoCompleteTextView mAutoCompleteSearchBar;
 
-    public static Intent newIntent(Context context){
+    public static Intent newIntent(Context context) {
         Intent intent = new Intent(context, LectureReviewSearchActivity.class);
         return intent;
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecture_review_search);
 
-        mAutoCompleteSearchBar = (AutoCompleteTextView)findViewById(R.id.lecture_review_search_bar);
+        mAutoCompleteSearchBar = (AutoCompleteTextView) findViewById(R.id.lecture_review_search_bar);
 
-        List<String> list = new ArrayList<>(Arrays.asList("가 나", "가나", "가나다"));
+        updateAuthComplete();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String lecture = (String) parent.getItemAtPosition(position);
+        Intent intent = LectureReviewListActivity.newIntent(this, lecture);
+        startActivity(intent);
+    }
+
+    public void updateAuthComplete(){
+        // 여기를 파베와 연동
+        List<String> list = new ArrayList<>(Arrays.asList("가 나", "나", "가나다"));
         int layoutItemId = android.R.layout.simple_dropdown_item_1line;
         LectureSearchAdapter adapter = new LectureSearchAdapter(this, layoutItemId, list);
         mAutoCompleteSearchBar.setAdapter(adapter);
+        mAutoCompleteSearchBar.setThreshold(1);
+        mAutoCompleteSearchBar.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAutoCompleteSearchBar.setText("");
     }
 }
---
+++ app/src/main/java/com/lifekau/android/lifekau/LectureSearchAdapter.java
@@ -0,0 +1,121 @@
+package com.lifekau.android.lifekau;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sgc109 on 2018-02-01.
+ */
+
+public class LectureSearchAdapter extends ArrayAdapter<String> {
+    private LayoutInflater layoutInflater;
+    List<String> mLectures;
+
+    private Filter mFilter = new Filter() {
+        @Override
+        public String convertResultToString(Object resultValue) {
+            return ((String)resultValue);
+        }
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint != null) {
+                ArrayList<String> suggestions = new ArrayList<String>();
+                String keyword = constraint.toString();
+                int last = -1;
+                for(int i = keyword.length() - 1; i >= 0; i--){
+                    if(keyword.charAt(i) != ' ') {
+                        last = i;
+                        break;
+                    }
+                }
+                if(last == -1) return results;
+
+                keyword = keyword.substring(0, last + 1);
+                for (String lecture : mLectures) {
+                    boolean ok = true;
+                    int pos1 = 0;
+                    int pos2 = 0;
+                    while(pos1 < keyword.length() && pos2 < lecture.length()){
+                        if(keyword.charAt(pos1) == ' ') pos1++;
+                        else if(lecture.charAt(pos2) == ' ') pos2++;
+                        else if(keyword.charAt(pos1) == lecture.charAt(pos2)) {
+                            pos1++;
+                            pos2++;
+                        }
+                        else {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if(!ok || pos1 < keyword.length()) continue;
+                    suggestions.add(lecture);
+                }
+
+                results.values = suggestions;
+                results.count = suggestions.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            clear();
+            if (results != null && results.count > 0) {
+                // we have filtered results
+                addAll((ArrayList<String>) results.values);
+            } else {
+                // no filter, add entire original list back in
+                addAll(mLectures);
+            }
+            notifyDataSetChanged();
+        }
+    };
+
+    public LectureSearchAdapter(Context context, int textViewResourceId, List<String> customers) {
+        super(context, textViewResourceId, customers);
+        // copy all the customers into a master list
+        mLectures = new ArrayList<String>(customers.size());
+        mLectures.addAll(customers);
+        layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        View view = convertView;
+//
+////        view.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////
+////            }
+////        });
+////        if (view == null) {
+////            view = layoutInflater.inflate(R.layout.customerNameLabel, null);
+////        }
+////
+////        String customer = getItem(position);
+////
+////        TextView name = (TextView) view.findViewById(R.id.customerNameLabel);
+////        name.setText(customer.getName());
+//
+//        return view;
+//    }
+
+    @Override
+    public Filter getFilter() {
+        return mFilter;
+    }
+}
