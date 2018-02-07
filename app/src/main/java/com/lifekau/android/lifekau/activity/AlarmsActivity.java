e8d61d66ddbedc22f6e90ae252a58994826d3f17
sgc109 <sgc109109@gmail.com>
2018-02-08 01:22:26 +0900

    머지 후 중복 클래스 삭제(한글 관련, 설정 액티비티)

--- app/src/main/java/com/lifekau/android/lifekau/activity/AlarmsActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/activity/AlarmsActivity.java
@@ -1,18 +1,20 @@
-package com.lifekau.android.lifekau;
+package com.lifekau.android.lifekau.activity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.lifekau.android.lifekau.R;
+
+public class AlarmsActivity extends AppCompatActivity {
     public static Intent newIntent(Context context){
-        Intent intent = new Intent(context, SettingsActivity.class);
+        Intent intent = new Intent(context, AlarmsActivity.class);
         return intent;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_alarms);
     }
 }
