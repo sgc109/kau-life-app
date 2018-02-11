68a34c49d3ed4140627fa448e46574ef40413062
sgc109 <sgc109109@gmail.com>
2018-02-11 18:26:40 +0900

    커뮤니티 글 뷰랑 파베연동 FirebaseRecyclerAdapter 이용해서 좀했음(pagination 구현때매 파베리사이클러어댑터 안쓰고하는걸로 바꿀듯)

--- app/src/main/java/com/lifekau/android/lifekau/fragment/CommunityFragment.java
+++ app/src/main/java/com/lifekau/android/lifekau/fragment/CommunityFragment.java
@@ -15,7 +15,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -27,10 +30,14 @@ import com.lifekau.android.lifekau.R;
 import com.lifekau.android.lifekau.activity.PostDetailActivity;
 import com.lifekau.android.lifekau.model.Post;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class CommunityFragment extends PagerFragment {
 
     private DatabaseReference mDatabase;
     private RecyclerView mRecyclerView;
+    private ProgressBar mProgressBar;
     private FirebaseRecyclerAdapter<Post, PostViewHolder> mAdapter;
 
     public CommunityFragment() {
@@ -49,6 +56,8 @@ public class CommunityFragment extends PagerFragment {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mRecyclerView = view.findViewById(R.id.community_recycler_view);
+        mProgressBar = view.findViewById(R.id.post_list_progress_bar);
+        mRecyclerView.setHasFixedSize(true);
 
         return view;
     }
@@ -56,6 +65,9 @@ public class CommunityFragment extends PagerFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mProgressBar.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.GONE);
 
         Query postsQuery = mDatabase.child(getString(R.string.firebase_database_posts))
                 .limitToFirst(100);
@@ -69,6 +81,13 @@ public class CommunityFragment extends PagerFragment {
             private final int NOT_UPPER_MOST_POST = 2;
 
             @Override
+            public void onDataChanged() {
+                super.onDataChanged();
+                mProgressBar.setVisibility(View.GONE);
+                mRecyclerView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
             public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_post, parent, false);
                 if (viewType == UPPER_MOST_POST) {
@@ -76,7 +95,7 @@ public class CommunityFragment extends PagerFragment {
                             LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT
                     );
-                    params.setMargins(0, convertDpToPx(56), 0, 0);
+                    params.setMargins(0, convertDpToPx(72), 0, 0);
                     view.setLayoutParams(params);
                 }
                 return new PostViewHolder(view);
@@ -85,33 +104,38 @@ public class CommunityFragment extends PagerFragment {
             @Override
             public void onBindViewHolder(PostViewHolder holder, int position, final Post model) {
                 final String postKey = getRef(position).getKey();
-                holder.bind(model, new View.OnClickListener() {
+                holder.mTextView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         Intent intent = PostDetailActivity.newIntent(CommunityFragment.this.getContext(), postKey);
                         startActivity(intent);
                     }
                 });
+                holder.bind(model);
             }
 
             @Override
             public int getItemViewType(int position) {
-                if (position == 0) return UPPER_MOST_POST;
+                if (position == getItemCount() - 1) return UPPER_MOST_POST;
                 return NOT_UPPER_MOST_POST;
             }
         };
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setReverseLayout(true);
+        manager.setStackFromEnd(true);
+
+        mRecyclerView.setLayoutManager(manager);
         mRecyclerView.setAdapter(mAdapter);
         setHasOptionsMenu(true);
     }
 
-    private int convertDpToPx(int dp){
-        return Math.round(dp*(getResources().getDisplayMetrics().xdpi/ DisplayMetrics.DENSITY_DEFAULT));
+    private int convertDpToPx(int dp) {
+        return Math.round(dp * (getResources().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
 
     }
 
-    private int convertPxToDp(int px){
-        return Math.round(px/(Resources.getSystem().getDisplayMetrics().xdpi/DisplayMetrics.DENSITY_DEFAULT));
+    private int convertPxToDp(int px) {
+        return Math.round(px / (Resources.getSystem().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     @Override
@@ -121,19 +145,37 @@ public class CommunityFragment extends PagerFragment {
     }
 
     class PostViewHolder extends RecyclerView.ViewHolder {
-        CardView mCardView;
-        TextView mTextView;
+        public CardView mCardView;
+        public TextView mTextView;
+        public ImageButton mLikeButton;
+        public ImageView mCommentButton;
+        public TextView mCommentCountTextView;
+        public TextView mLikeCountTextView;
 
         public PostViewHolder(View itemView) {
             super(itemView);
-            mCardView = itemView.findViewById(R.id.card_view);
-            mTextView = itemView.findViewById(R.id.layout_item_demo_title);
+            mCardView = itemView.findViewById(R.id.list_item_post_card_view);
+            mTextView = itemView.findViewById(R.id.list_item_post_content_text_view);
+            mLikeButton = itemView.findViewById(R.id.list_item_post_like_image_button);
+            mCommentButton = itemView.findViewById(R.id.list_item_post_comment_image_button);
+            mCommentCountTextView = itemView.findViewById(R.id.list_item_post_comment_count_text_view);
+            mLikeCountTextView = itemView.findViewById(R.id.list_item_post_like_count);
         }
 
-        public void bind(Post post, View.OnClickListener listener) {
-            itemView.setOnClickListener(listener);
+        public void bind(Post post) {
             mTextView.setText(post.text);
-            // post 내용으로 뷰 세팅하기
+            mCommentCountTextView.setText(String.format(
+                    getString(R.string.post_comment_count),
+                    NumberFormat.getNumberInstance(Locale.US).format(post.commentCount)));
+            mLikeCountTextView.setText(NumberFormat.getNumberInstance(Locale.US).format(post.likeCount));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdapter != null) {
+            mAdapter.notifyDataSetChanged();
         }
     }
 
--- app/src/main/java/com/lifekau/android/lifekau/model/Post.java
+++ app/src/main/java/com/lifekau/android/lifekau/model/Post.java
@@ -8,12 +8,14 @@ import java.util.Date;
 
 public class Post {
     public String author;
+    public int commentCount;
     public int likeCount;
     public String text;
     public long date;
     public Post(){}
     public Post(String author, String text){
         this.author = author;
+        this.commentCount = 0;
         this.likeCount = 0;
         this.text = text;
         this.date = new Date().getTime();
