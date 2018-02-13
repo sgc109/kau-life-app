c80d45eb1adc0faf88e6672de41016de4d0747fd
sgc109 <sgc109109@gmail.com>
2018-02-13 23:59:59 +0900

    댓글 뷰 변경

--- app/src/main/java/com/lifekau/android/lifekau/viewholder/CommentViewHolder.java
+++ app/src/main/java/com/lifekau/android/lifekau/viewholder/CommentViewHolder.java
@@ -1,17 +1,83 @@
 package com.lifekau.android.lifekau.viewholder;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import com.lifekau.android.lifekau.DateDisplayer;
+import com.lifekau.android.lifekau.R;
+import com.lifekau.android.lifekau.manager.LoginManager;
+import com.lifekau.android.lifekau.model.Comment;
+import com.lifekau.android.lifekau.model.Post;
+
+import java.util.Date;
 
 /**
  * Created by sgc109 on 2018-02-12.
  */
 
-public class CommentViewHolder extends RecyclerView.ViewHolder
-    private TextView mTextView;
+public class CommentViewHolder extends RecyclerView.ViewHolder{
+    private TextView mContentTextView;
     private TextView mDateTextView;
-    public CommentViewHolder(View itemView) {
+    private TextView mLikeCountTextView;
+    private ImageView mLikeImageView;
+    private Context mContext;
+    public CommentViewHolder(View itemView, Context context) {
         super(itemView);
+        mContentTextView = itemView.findViewById(R.id.list_item_comment_content_text_view);
+        mDateTextView = itemView.findViewById(R.id.list_item_comment_date_text_view);
+        mLikeCountTextView = itemView.findViewById(R.id.list_item_comment_like_count_text_view);
+        mLikeImageView = itemView.findViewById(R.id.list_item_comment_like_image_view);
+    }
+    public void bind(Comment comment){
+        mContentTextView.setText(comment.text);
+        mDateTextView.setText(DateDisplayer.dateToStringFormat(new Date(comment.date)));
+        mLikeCountTextView.setText(comment.likeCount);
+        if(comment.likeCount == 0){
+            mLikeImageView.setVisibility(View.GONE);
+            mLikeCountTextView.setVisibility(View.GONE);
+        } else {
+            mLikeImageView.setVisibility(View.VISIBLE);
+            mLikeCountTextView.setVisibility(View.VISIBLE);
+        }
+
+    }
+    private void onLikeClicked(DatabaseReference postRef) {
+        final String studentId = LoginManager.get(mContext).getStudentId();
+        postRef.runTransaction(new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                Post p = mutableData.getValue(Post.class);
+                if (p == null) {
+                    return Transaction.success(mutableData);
+                }
+
+                if (p.likes.containsKey(studentId)) {
+                    p.likeCount--;
+                    p.likes.remove(studentId);
+                } else {
+                    p.likeCount++;
+                    p.likes.put(studentId, true);
+                }
+
+                mutableData.setValue(p);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b,
+                                   DataSnapshot dataSnapshot) {
+                // Transaction completed
+                Log.d("fuck", "postTransaction:onComplete:" + databaseError);
+            }
+        });
     }
 }
--- app/src/main/java/com/lifekau/android/lifekau/viewholder/PostViewHolder.java
+++ app/src/main/java/com/lifekau/android/lifekau/viewholder/PostViewHolder.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.MutableData;
 import com.google.firebase.database.Transaction;
+import com.lifekau.android.lifekau.DateDisplayer;
 import com.lifekau.android.lifekau.R;
 import com.lifekau.android.lifekau.activity.PostDetailActivity;
 import com.lifekau.android.lifekau.manager.LoginManager;
@@ -112,7 +113,7 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
                 mContext.getString(R.string.post_comment_count),
                 NumberFormat.getNumberInstance(Locale.US).format(mPost.commentCount)));
         mLikeCountTextView.setText(NumberFormat.getNumberInstance(Locale.US).format(mPost.likeCount));
-        mDateTextView.setText(dateToString(new Date(mPost.date)));
+        mDateTextView.setText(DateDisplayer.dateToStringFormat(new Date(mPost.date)));
         if (mPost.likeCount == 0) {
             mLikeCountTextView.setVisibility(View.GONE);
             mCircleHeartImageView.setVisibility(View.GONE);
@@ -171,44 +172,5 @@ public class PostViewHolder extends RecyclerView.ViewHolder {
     private void startDetailActivity() {
         Intent intent = PostDetailActivity.newIntent(mContext, mPostKey);
         mContext.startActivity(intent);
-    }
-
-    private String dateToString(Date past) {
-        Date now = new Date();
-        SimpleDateFormat toYear = new SimpleDateFormat("yyyy");
-        SimpleDateFormat toMonth = new SimpleDateFormat("M");
-        SimpleDateFormat toDay = new SimpleDateFormat("d");
-
-        int nowYear = Integer.parseInt(toYear.format(now));
-        int pastYear = Integer.parseInt(toYear.format(past));
-        int nowMonth = Integer.parseInt(toMonth.format(now));
-        int pastMonth = Integer.parseInt(toMonth.format(past));
-        int nowDay = Integer.parseInt(toDay.format(now));
-        int pastDay = Integer.parseInt(toDay.format(past));
-
-        if (nowYear != pastYear) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 M월 d일 a h:mm");
-            return formatter.format(past);
-        } else if (nowMonth != pastMonth
-                || nowDay >= pastDay + 2) {
-            SimpleDateFormat formatter = new SimpleDateFormat("M월 d일 a h:mm");
-            return formatter.format(past);
-        } else if (nowDay >= pastDay + 1) {
-            SimpleDateFormat formatter = new SimpleDateFormat("어제 a h:mm");
-            return formatter.format(past);
-        }
-        long diff = now.getTime() - past.getTime();
-        diff /= 1000;
-        long hour = diff / 3600;
-        long minute = diff / 60;
-
-        if (minute == 0) {
-            return "방금 전";
-        } else if (hour == 0) {
-            return "" + minute + "분 전";
-        } else {
-            SimpleDateFormat formatter = new SimpleDateFormat("a h:mm");
-            return formatter.format(past);
-        }
     }
 }
