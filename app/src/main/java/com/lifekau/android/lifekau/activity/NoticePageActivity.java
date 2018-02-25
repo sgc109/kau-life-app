224f340be72a8c894f92c96c1d533ddb1c66478f
sgc109 <sgc109109@gmail.com>
2018-02-26 02:21:21 +0900

    공지 페이지 액티비티 newIntent static 메서드 추가

--- app/src/main/java/com/lifekau/android/lifekau/activity/NoticePageActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/activity/NoticePageActivity.java
@@ -1,5 +1,6 @@
 package com.lifekau.android.lifekau.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,15 @@ import android.webkit.WebView;
 import com.lifekau.android.lifekau.R;
 
 public class NoticePageActivity extends AppCompatActivity {
+    private static String EXTRA_URL = "extra_url";
+    private static String EXTRA_POST = "extra_post";
+
+    public static Intent newIntent(Context context, String url, String post){
+        Intent intent = new Intent(context, NoticePageActivity.class);
+        intent.putExtra(EXTRA_URL, url);
+        intent.putExtra(EXTRA_POST, post);
+        return intent;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +32,7 @@ public class NoticePageActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.notice_page);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
-        webView.postUrl(intent.getStringExtra("URL"), intent.getStringExtra("POST").getBytes());
+        webView.postUrl(intent.getStringExtra(EXTRA_URL), intent.getStringExtra(EXTRA_POST).getBytes());
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
