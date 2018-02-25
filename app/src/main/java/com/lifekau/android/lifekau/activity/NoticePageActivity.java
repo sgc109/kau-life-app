0f5444b46e85d6d391bf60929f1e3e42f7d54eef
sgc109 <sgc109109@gmail.com>
2018-02-26 02:14:45 +0900

    웹뷰 확대 축소 활성화, 확대 축소 버튼 감추기

--- app/src/main/java/com/lifekau/android/lifekau/activity/NoticePageActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/activity/NoticePageActivity.java
@@ -3,6 +3,7 @@ package com.lifekau.android.lifekau.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 
 import com.lifekau.android.lifekau.R;
@@ -12,13 +13,18 @@ public class NoticePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_page);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
         Intent intent = getIntent();
         WebView webView = findViewById(R.id.notice_page);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
         webView.postUrl(intent.getStringExtra("URL"), intent.getStringExtra("POST").getBytes());
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
     }
 }
