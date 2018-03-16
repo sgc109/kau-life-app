36817b6d91c83ce12137df02af6e3e32ffcfb147
taemin <cprayer@naver.com>
2018-03-16 17:20:36 +0900

    외부 라이브러리 버전 업데이트 후 발생 문제 해결(파이어베이스 버전 수정) / 식단표 구현

--- app/src/main/java/com/lifekau/android/lifekau/activity/FoodMenuActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/activity/FoodMenuActivity.java
@@ -1,8 +1,209 @@
 package com.lifekau.android.lifekau.activity;
 
-/**
- * Created by taemin on 2018-03-15.
- */
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class FoodMenuActivity {
+import com.lifekau.android.lifekau.R;
+import com.lifekau.android.lifekau.manager.NoticeManager;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
+import static android.os.SystemClock.sleep;
+
+public class FoodMenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private NoticeManager mNoticeManager = NoticeManager.getInstance();
+    private WebView mFoodMenuWebView;
+    private Button mShowStudentRestFoodMenuButton;
+    private Button mShowDormitoryRestFoodMenuButton;
+
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, FoodMenuActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_menu);
+
+        if (getSupportActionBar() != null) {
+//            getSupportActionBar().hide();
+            getSupportActionBar().setTitle("식단표");
+        }
+
+        mFoodMenuWebView = findViewById(R.id.activity_food_menu_web_view);
+        mFoodMenuWebView.setInitialScale(1);
+        mFoodMenuWebView.getSettings().setLoadWithOverviewMode(true);
+        mFoodMenuWebView.getSettings().setUseWideViewPort(true);
+        mFoodMenuWebView.getSettings().setSupportZoom(true);
+        mFoodMenuWebView.getSettings().setBuiltInZoomControls(true);
+        mFoodMenuWebView.getSettings().setDisplayZoomControls(false);
+        mShowStudentRestFoodMenuButton = findViewById(R.id.activity_food_menu_show_student_restaurant_button);
+        mShowStudentRestFoodMenuButton.setOnClickListener(this);
+        mShowDormitoryRestFoodMenuButton = findViewById(R.id.activity_food_menu_dormitory_restaurant_button);
+        mShowDormitoryRestFoodMenuButton.setOnClickListener(this);
+        showStudentRestFoodMenu();
+    }
+
+    void showStudentRestFoodMenu(){
+        mFoodMenuWebView.setInitialScale(1);
+        GetStudentRestMenuASyncTask aSyncTask = new GetStudentRestMenuASyncTask(getApplication(), this);
+        aSyncTask.execute();
+    }
+
+    void showDormitoryRestFoodMenu(){
+        mFoodMenuWebView.setInitialScale(1);
+        GetDormitoryRestMenuASyncTask aSyncTask = new GetDormitoryRestMenuASyncTask(getApplication(), this);
+        aSyncTask.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.activity_food_menu_show_student_restaurant_button:
+                showStudentRestFoodMenu();
+                break;
+            case R.id.activity_food_menu_dormitory_restaurant_button:
+                showDormitoryRestFoodMenu();
+                break;
+        }
+    }
+
+    public void setFoodMenuImage(Set<String> fileNames){
+        String[] fileNameArray = fileNames.toArray(new String[fileNames.size()]);
+        Arrays.sort(fileNameArray);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html><table>");
+        for (String fileName : fileNameArray) {
+            stringBuilder.append("<tr><td><img src= 'file://");
+            stringBuilder.append(getFilesDir().getAbsolutePath());
+            stringBuilder.append("/");
+            stringBuilder.append(fileName);
+            stringBuilder.append("'/></td></tr>");
+        }
+        stringBuilder.append("</table></html>");
+        Log.e("얻어지는 것", stringBuilder.toString());
+        mFoodMenuWebView.loadDataWithBaseURL("",
+                stringBuilder.toString(),
+                "text/html",
+                "utf-8",
+                null);
+    }
+
+    private static class GetStudentRestMenuASyncTask extends AsyncTask<Integer, Void, Integer> {
+
+        private WeakReference<FoodMenuActivity> activityReference;
+        private WeakReference<Application> applicationWeakReference;
+
+        GetStudentRestMenuASyncTask(Application application, FoodMenuActivity foodMenuActivity) {
+            applicationWeakReference = new WeakReference<>(application);
+            activityReference = new WeakReference<>(foodMenuActivity);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            FoodMenuActivity foodMenuActivity = activityReference.get();
+            Resources resources = applicationWeakReference.get().getResources();
+            if (foodMenuActivity == null || foodMenuActivity.isFinishing())
+                return resources.getInteger(R.integer.unexpected_error);
+            int result;
+            int count = 0;
+            NoticeManager nm = foodMenuActivity.mNoticeManager;
+            while ((result = nm.pullStudentRestFoodMenu(applicationWeakReference.get())) != resources.getInteger(R.integer.no_error) && !isCancelled()) {
+                if (result == resources.getInteger(R.integer.network_error)) {
+                    sleep(1000);
+                    count++;
+                } else return result;
+                if (count == resources.getInteger(R.integer.maximum_retry_num))
+                    return resources.getInteger(R.integer.network_error);
+            }
+            return 0;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            FoodMenuActivity foodMenuActivity = activityReference.get();
+            Resources resources = applicationWeakReference.get().getResources();
+            if (foodMenuActivity == null || foodMenuActivity.isFinishing()) return;
+            NoticeManager nm = foodMenuActivity.mNoticeManager;
+            if (result == resources.getInteger(R.integer.file_write_error)) {
+                foodMenuActivity.showToast("식단표 파일을 저장하는데 실패하였습니다.");
+                return;
+            } else if (result == resources.getInteger(R.integer.network_error)) {
+                foodMenuActivity.showToast("네트워크 오류로 인해 최신 식단표 파일을 가져오는데 실패하였습니다.");
+            }
+            foodMenuActivity.setFoodMenuImage(nm.getStudentRestFoodMenuFileName());
+        }
+    }
+
+    private static class GetDormitoryRestMenuASyncTask extends AsyncTask<Integer, Void, Integer> {
+
+        private WeakReference<FoodMenuActivity> activityReference;
+        private WeakReference<Application> applicationWeakReference;
+
+        GetDormitoryRestMenuASyncTask(Application application, FoodMenuActivity foodMenuActivity) {
+            applicationWeakReference = new WeakReference<>(application);
+            activityReference = new WeakReference<>(foodMenuActivity);
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            FoodMenuActivity foodMenuActivity = activityReference.get();
+            Resources resources = applicationWeakReference.get().getResources();
+            if (foodMenuActivity == null || foodMenuActivity.isFinishing())
+                return resources.getInteger(R.integer.unexpected_error);
+            int result;
+            int count = 0;
+            NoticeManager nm = foodMenuActivity.mNoticeManager;
+            while ((result = nm.pullDormitoryRestFoodMenu(applicationWeakReference.get())) != resources.getInteger(R.integer.no_error) && !isCancelled()) {
+                if (result == resources.getInteger(R.integer.network_error)) {
+                    sleep(1000);
+                    count++;
+                } else return result;
+                if (count == resources.getInteger(R.integer.maximum_retry_num))
+                    return resources.getInteger(R.integer.network_error);
+            }
+            return 0;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            FoodMenuActivity foodMenuActivity = activityReference.get();
+            Resources resources = applicationWeakReference.get().getResources();
+            if (foodMenuActivity == null || foodMenuActivity.isFinishing()) return;
+            NoticeManager nm = foodMenuActivity.mNoticeManager;
+            if (result == resources.getInteger(R.integer.file_write_error)) {
+                foodMenuActivity.showToast("식단표 파일을 저장하는데 실패하였습니다.");
+                return;
+            } else if (result == resources.getInteger(R.integer.network_error)) {
+                foodMenuActivity.showToast("네트워크 오류로 인해 최신 식단표 파일을 가져오는데 실패하였습니다.");
+            }
+            foodMenuActivity.setFoodMenuImage(nm.getDormitoryRestFileName());
+        }
+    }
+
+    public void showToast(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
--- app/src/main/java/com/lifekau/android/lifekau/manager/NoticeManager.java
+++ app/src/main/java/com/lifekau/android/lifekau/manager/NoticeManager.java
@@ -3,24 +3,30 @@ package com.lifekau.android.lifekau.manager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import com.lifekau.android.lifekau.R;
 import com.lifekau.android.lifekau.model.Notice;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.lang.reflect.Array;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
+import java.util.TreeSet;
 
 import okhttp3.Call;
 import okhttp3.FormBody;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -30,9 +36,10 @@ public class NoticeManager {
 
 
     private final static int TOTAL_NOTICE_NUM = 5;
-    private final static int TOTAL_CARTE_NUM = 2;
-    private static final String SAVE_STUDENT_RESTAURANT_DETAIL_NUM = "shared_preference_student_restaurant_detail_num";
-    private static final String SAVE_DORMITORY_RESTAURANT_DETAIL_NUM = "shared_preference_dormitory_restaurant_detail_num";
+    private static final String SAVE_STUDENT_REST_FILE_NAME = "shared_preference_dormitory_restaurant_file_name";
+    private static final String SAVE_STUDENT_REST_DETAIL_NUM = "shared_preference_student_restaurant_detail_num";
+    private static final String SAVE_DORMITORY_REST_FILE_NAME = "shared_preference_dormitory_restaurant_file_name";
+    private static final String SAVE_DORMITORY_REST_DETAIL_NUM = "shared_preference_dormitory_restaurant_detail_num";
     private final static String[] COMMUNITY_KEY = {"B0146", "B0147", "B0230", "B0259", "B0148"};
     private final static String[] URL_TYPE = {"/page/kauspace/", "/page/kau_media/"};
     private final static String[] NOTICE_LIST = {"general_list", "academicinfo_list", "scholarship_list", "career_list", "event_list"};
@@ -43,7 +50,8 @@ public class NoticeManager {
     private int[] mLatestDetailPageNum;
     private ArrayList<Notice>[] mImportantNoticeList;
     private Hashtable<Integer, Notice>[] mNoticeListMap;
-    private ArrayList<String> mFoodMenuUrl;
+    private Set<String> mStudentRestFileName;
+    private Set<String> mDormitoryRestFileName;
 
     private NoticeManager() {
         mAllPageFetched = new boolean[TOTAL_NOTICE_NUM];
@@ -52,8 +60,8 @@ public class NoticeManager {
         mLatestDetailPageNum = new int[TOTAL_NOTICE_NUM];
         mNoticeListMap = new Hashtable[TOTAL_NOTICE_NUM];
         mImportantNoticeList = new ArrayList[TOTAL_NOTICE_NUM];
-        mLatestDetailPageNum = new int[TOTAL_CARTE_NUM];
-        mFoodMenuUrl = new ArrayList<>();
+        mStudentRestFileName = new TreeSet<>();
+        mDormitoryRestFileName = new TreeSet<>();
         for (int i = 0; i < TOTAL_NOTICE_NUM; i++) mNoticeListMap[i] = new Hashtable<>();
         for (int i = 0; i < TOTAL_NOTICE_NUM; i++) mImportantNoticeList[i] = new ArrayList<>();
     }
@@ -191,13 +199,12 @@ public class NoticeManager {
         return stringBuffer.toString();
     }
 
-    public int pullStudentRestaurantFoodMenu(Context context) {
+    public int pullStudentRestFoodMenu(Context context) {
         Resources resources = context.getResources();
         SharedPreferences sharedPref = context.getSharedPreferences(resources.getString(R.string.shared_preference_app), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url(resources.getString(R.string.food_menu_page))
+                .url(resources.getString(R.string.food_menu_student_rest_page))
                 .build();
         Call call = client.newCall(request);
         int currIndex = -1;
@@ -221,12 +228,11 @@ public class NoticeManager {
             e.printStackTrace();
             return resources.getInteger(R.integer.network_error);
         }
-        int prevIndex = sharedPref.getInt(SAVE_STUDENT_RESTAURANT_DETAIL_NUM, -1);
-        if(currIndex == prevIndex) return resources.getInteger(R.integer.no_error);
         request = new Request.Builder()
-                .url(String.format(resources.getString(R.string.food_menu_view_page), currIndex))
+                .url(String.format(resources.getString(R.string.food_menu_student_rest_view_page), currIndex))
                 .build();
         call = client.newCall(request);
+        ArrayList<String> foodMenuUrls = new ArrayList<>();
         try (Response res = call.execute()) {
             if (res.code() <= 199 || res.code() >= 301) {
                 return resources.getInteger(R.integer.server_error);
@@ -234,8 +240,8 @@ public class NoticeManager {
             Document doc = Jsoup.parse(res.body().string());
             Elements fileElements = doc.getElementsByAttributeValue("class", "tb01_3").get(1).select("a");
             if (fileElements.size() > 0) {
-                for (Element e : fileElements){
-                    mFoodMenuUrl.add(resources.getString(R.string.portal_page) + e .attr("href"));
+                for (Element e : fileElements) {
+                    foodMenuUrls.add(resources.getString(R.string.portal_page) + e.attr("href"));
                 }
             }
         } catch (IndexOutOfBoundsException | NullPointerException e) {
@@ -245,15 +251,152 @@ public class NoticeManager {
             e.printStackTrace();
             return resources.getInteger(R.integer.network_error);
         }
-        return 0;
+        int prevIndex = sharedPref.getInt(SAVE_STUDENT_REST_DETAIL_NUM, -1);
+        mStudentRestFileName = sharedPref.getStringSet(SAVE_STUDENT_REST_FILE_NAME, new HashSet<String>());
+        if (currIndex == prevIndex && foodMenuUrls.size() == mStudentRestFileName.size()) return resources.getInteger(R.integer.no_error);
+        mStudentRestFileName.clear();
+        int count = 0;
+        for (String url : foodMenuUrls) {
+            request = new Request.Builder()
+                    .url(url)
+                    .build();
+            call = client.newCall(request);
+            try (Response res = call.execute()) {
+                String fileName = "s" + count + ".jpg";
+                count++;
+                mStudentRestFileName.add(fileName);
+                byte[] imageBytes = res.body().bytes();
+                Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 10, baos);
+                File file = new File(context.getFilesDir(), fileName);
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(baos.toByteArray());
+                fos.close();
+            } catch (IOException e) {
+                return resources.getInteger(R.integer.file_write_error);
+            } catch (Exception e) {
+                return resources.getInteger(R.integer.network_error);
+            }
+        }
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(SAVE_STUDENT_REST_DETAIL_NUM, currIndex);
+        editor.putStringSet(SAVE_STUDENT_REST_FILE_NAME, mStudentRestFileName);
+        editor.apply();
+        return resources.getInteger(R.integer.no_error);
     }
 
-    public ArrayList<String> getFoodFileUrls() {
-        return mFoodMenuUrl;
+    public int pullDormitoryRestFoodMenu(Context context) {
+        Resources resources = context.getResources();
+        SharedPreferences sharedPref = context.getSharedPreferences(resources.getString(R.string.shared_preference_app), Context.MODE_PRIVATE);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(resources.getString(R.string.food_menu_dormitory_rest_page))
+                .build();
+        Call call = client.newCall(request);
+        int currIndex = -1;
+        try (Response res = call.execute()) {
+            if (res.code() <= 199 || res.code() >= 301) {
+                return resources.getInteger(R.integer.server_error);
+            }
+            Document doc = Jsoup.parse(res.body().string());
+            Elements postElements = doc.getElementsByAttributeValue("headers", "board_title");
+            for (Element e : postElements) {
+                if (e.text().contains("월")) {
+                    String string = e.select("a").get(0).attr("href");
+                    currIndex = Integer.valueOf(string.split("\\(")[1].split(",")[0]);
+                    break;
+                }
+            }
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            e.printStackTrace();
+            return resources.getInteger(R.integer.missing_data_error);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return resources.getInteger(R.integer.network_error);
+        }
+        RequestBody body = new FormBody.Builder()
+                .add("communityKey", "B0126")
+                .add("pageNum", "1")
+                .add("pageSize", "10")
+                .add("act", "VIEW")
+                .add("boardId", String.valueOf(currIndex))
+                .add("branch_session", "")
+                .add("only_reply", "")
+                .add("mbo_mother_page", "/page/web/life/community/meal_li.jsp")
+                .add("board_table_name", "WCM_BOARD_" + "B0126")
+                .add("sort_type", "DESC")
+                .add("sort_column", "")
+                .add("memoTable", "WCM_BOARD_MEMO" + "B0126")
+                .add("login_id", "")
+                .add("searchType", "TITLE")
+                .add("searchWord", "")
+                .add("chg_page_size", "10")
+                .build();
+        request = new Request.Builder()
+                .url(resources.getString(R.string.food_menu_dormitory_rest_page))
+                .post(body)
+                .build();
+        call = client.newCall(request);
+        ArrayList<String> foodMenuUrls = new ArrayList<>();
+        try (Response res = call.execute()) {
+            if (res.code() <= 199 || res.code() >= 301) {
+                return resources.getInteger(R.integer.server_error);
+            }
+            Document doc = Jsoup.parse(res.body().string());
+            Elements fileElements = doc.getElementsByAttributeValueStarting("href", "/page/cms/board/Download.jsp");
+            if (fileElements.size() > 0) {
+                for (Element e : fileElements) {
+                    foodMenuUrls.add(resources.getString(R.string.portal_page) + e.attr("href"));
+                }
+            }
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            e.printStackTrace();
+            return resources.getInteger(R.integer.missing_data_error);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return resources.getInteger(R.integer.network_error);
+        }
+        int prevIndex = sharedPref.getInt(SAVE_DORMITORY_REST_DETAIL_NUM, -1);
+        mDormitoryRestFileName = sharedPref.getStringSet(SAVE_DORMITORY_REST_FILE_NAME, new HashSet<String>());
+        if (currIndex == prevIndex && foodMenuUrls.size() == mDormitoryRestFileName.size()) return resources.getInteger(R.integer.no_error);
+        mDormitoryRestFileName.clear();
+        int count = 0;
+        for (String url : foodMenuUrls) {
+            request = new Request.Builder()
+                    .url(url)
+                    .build();
+            call = client.newCall(request);
+            try (Response res = call.execute()) {
+                String fileName = "d" + count + ".jpg";
+                count++;
+                mDormitoryRestFileName.add(fileName);
+                byte[] imageBytes = res.body().bytes();
+                Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 10, baos);
+                File file = new File(context.getFilesDir(), fileName);
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(baos.toByteArray());
+                fos.close();
+            } catch (IOException e) {
+                return resources.getInteger(R.integer.file_write_error);
+            } catch (Exception e) {
+                return resources.getInteger(R.integer.network_error);
+            }
+        }
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putStringSet(SAVE_DORMITORY_REST_FILE_NAME, mDormitoryRestFileName);
+        editor.putInt(SAVE_DORMITORY_REST_DETAIL_NUM, currIndex);
+        editor.apply();
+        return resources.getInteger(R.integer.no_error);
+    }
+    
+    public Set<String> getStudentRestFoodMenuFileName(){
+        return mStudentRestFileName;
     }
 
-    public void clearFoodFileUrls(){
-        mFoodMenuUrl.clear();
+    public Set<String> getDormitoryRestFileName(){
+        return mDormitoryRestFileName;
     }
-
 }
