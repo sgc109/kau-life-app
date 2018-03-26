25da414d080408e9c716fa11ea1cc62980ee99d1
sgc109 <sgc109109@gmail.com>
2018-03-26 20:43:49 +0900

    실행시 초기화면 뜨는 시간 0.8초로 줄임

--- app/src/main/java/com/lifekau/android/lifekau/activity/MainActivity.java
+++ app/src/main/java/com/lifekau/android/lifekau/activity/MainActivity.java
@@ -32,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, noMainImage ? 0 : 1000);
+        }, noMainImage ? 0 : 800);
     }
 }
